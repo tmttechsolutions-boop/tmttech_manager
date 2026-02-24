aa3b76bee91647b6dcaa770cbbf4d7a76322b66f
Thales Martins <Thales.Martins@engenharia.local>
2026-02-23 22:52:14 -0300

    add server side logs for env vars

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,5 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-// Initialize the Supabase client
+console.log("Supabase URL is:", supabaseUrl ? "Defined" : "UNDEFINED", supabaseUrl);
+console.log("Supabase Anon Key is:", supabaseAnonKey ? "Defined" : "UNDEFINED");
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    console.error("URGENT: Supabase env vars are missing!");
+}
+
 export const supabase = createClient(supabaseUrl || 'https://placeholder.supabase.co', supabaseAnonKey || 'placeholder-key')
